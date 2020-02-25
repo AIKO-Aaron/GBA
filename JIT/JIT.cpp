7163ed7adb517d49c2e523aa512214f864ae24a0
ul5 <ferieninthailand5@protonmail.com>
2020-02-25 17:22:17 +0100

    Some changes in JIT & deleted gdb_history... twice

--- JIT/JIT.cpp
+++ JIT/JIT.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdint.h>
 #include <sys/mman.h>
+#include <cstring>
 
 typedef int (*jit_code)(int arg);
 
@@ -13,10 +14,32 @@ namespace JIT {
 
     public:
         Compiler(int max_length);
+        Compiler(const char *code, int len);
 
         void mov_eax_ebp_08();
-        void add_r0_1();
+        void add_rax_1();
         void ret();
+
+        inline void push_rax() { code[index++] = 0x50; }
+        inline void push_rbx() { code[index++] = 0x53; }
+        inline void push_rcx() { code[index++] = 0x51; }
+        inline void push_rdx() { code[index++] = 0x52; }
+
+        inline void pop_rax() { code[index++] = 0x58; }
+        inline void pop_rbx() { code[index++] = 0x5B; }
+        inline void pop_rcx() { code[index++] = 0x59; }
+        inline void pop_rdx() { code[index++] = 0x5A; }
+
+        inline void xor_rbx_rbx() { code[index++] = 0x48; code[index++] = 0x31; code[index++] = 0xDB; }
+
+        inline void add_rbx_rax() { code[index++] = 0x48; code[index++] = 0x01; code[index++] = 0xC3; }
+
+        inline void jnz(char amt) { code[index++] = 0x75; code[index++] = 0xfe - amt; }
+
+        inline void mov_rcx_rax() { code[index++] = 0x48; code[index++] = 0x89; code[index++] = 0xC1; }
+        inline void mov_rax_rbx() { code[index++] = 0x48; code[index++] = 0x89; code[index++] = 0xD8; }
+
+        inline void dec_rcx() { code[index++] = 0x48; code[index++] = 0x83; code[index++] = 0xE9; code[index++] = 0x01; }
 
         int exec(int arg);
     };
@@ -29,13 +52,21 @@ JIT::Compiler::Compiler(int max_length) {
     }
 }
 
+JIT::Compiler::Compiler(const char *c, int len) {
+    code = (uint8_t*) mmap(0, len, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if(code == (uint8_t*) -1) {
+        printf("Error: %d\n", errno);
+    }
+    memcpy(code, c, len);
+}
+
 void JIT::Compiler::mov_eax_ebp_08() {
     code[index++] = 0x89;
     code[index++] = 0xD0;
     //code[index++] = 0x18;
 }
 
-void JIT::Compiler::add_r0_1() {
+void JIT::Compiler::add_rax_1() {
     code[index++] = 0x83;
     code[index++] = 0xC0;
     code[index++] = 0x01;
@@ -49,14 +80,28 @@ int JIT::Compiler::exec(int arg) {
     return ((jit_code)code)(arg);
 }
 
+const char *code = "\x53\x51\x48\x89\xC1\x48\x89\xC3\x48\x83\xE9\x01\x74\x05\x48\x01\xD8\xEB\xF5\x59\x5B\xC3";
+
 int main(int argc, char** args) {
-    std::cout << args[0] << std::endl;
+    /*JIT::Compiler *c = new JIT::Compiler(100);
+    c->push_rbx();
+    c->push_rcx();
+    
+    c->mov_rcx_rax();
+    c->xor_rbx_rbx();
 
-    JIT::Compiler *c = new JIT::Compiler(4);
-    c->mov_eax_ebp_08();
-    c->add_r0_1();
-    c->ret();
+    c->add_rbx_rax();
+    c->dec_rcx();
+    c->jnz(7);
+    
+    c->mov_rax_rbx();
+    c->pop_rcx();
+    c->pop_rbx();
+    c->ret();*/
 
-    int ret = c->exec(0xFF);
-    printf("Got return vale: %.02X\n", ret);
+    JIT::Compiler *c = new JIT::Compiler(code, 23);
+    int ret = c->exec(16);
+    printf("Got return value: %.08X\n", ret);
+
+    delete c;
 }
